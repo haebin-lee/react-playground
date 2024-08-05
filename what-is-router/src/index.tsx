be4dde5aee5e5ed319a11065cc033dcdc088e226
haebin.lee <luckylucy0717@gmail.com>
2024-08-04 18:11:43 -0700

    practice createBrowserRouter

--- what-is-router/src/index.tsx
+++ what-is-router/src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { RouterProvider } from "react-router-dom";
+import Router from "./Router";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
@@ -8,7 +9,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={Router} />
   </React.StrictMode>
 );
 
