18ec6ee7842771364933c00818eb9f9cddb0e6e5
haebin.lee <luckylucy0717@gmail.com>
2024-08-04 18:06:00 -0700

    feat:  what-is-router initialize and practice browse router

---
+++ what-is-router/src/Router.tsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./screens/Home";
+import About from "./screens/About";
+
+const Router: React.FC = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default Router;
