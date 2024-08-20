767106513e4e2c14834857ee19f87930b41beeaa
haebin.lee <luckylucy0717@gmail.com>
2024-08-19 22:37:26 -0700

    git add apexcjart

--- crypto-tracker/src/routes/Chart.tsx
+++ crypto-tracker/src/routes/Chart.tsx
@@ -1,7 +1,99 @@
-function Chart() {
+import { useQuery } from "react-query";
+import { fetchCoinHistory } from "../api";
+import ApexChart from "react-apexcharts";
+import { format } from "path";
+
+interface IHistorical {
+  time_open: number;
+  time_close: number;
+  open: string;
+  high: string;
+  low: string;
+  close: string;
+  volume: string;
+  market_cap: number;
+}
+
+interface ChartProps {
+  coinId: string;
+}
+
+function Chart({ coinId }: ChartProps) {
+  const { isLoading, data } = useQuery<IHistorical[]>(
+    ["ohlcv", coinId],
+    () => fetchCoinHistory(coinId),
+    {
+      refetchInterval: 5000,
+    }
+  );
+
+  console.log(data);
   return (
     <>
-      <h1>Chart</h1>
+      <div>
+        {isLoading ? (
+          "Loading chart..."
+        ) : (
+          <ApexChart
+            type="line"
+            series={[
+              {
+                data: data?.map((price) => Number(price.close)) ?? [],
+                name: "Price",
+              },
+            ]}
+            options={{
+              theme: {
+                mode: "dark",
+              },
+              chart: {
+                background: "transparent",
+                height: 500,
+                width: 500,
+                toolbar: {
+                  show: false,
+                },
+              },
+              grid: {
+                show: false,
+              },
+              yaxis: {
+                show: false,
+              },
+              xaxis: {
+                labels: {
+                  show: false,
+                },
+                axisTicks: {
+                  show: false,
+                },
+                axisBorder: {
+                  show: false,
+                },
+                categories: data?.map((v) => v.time_close),
+                type: "datetime",
+              },
+              fill: {
+                type: "gradient",
+                gradient: {
+                  gradientToColors: ["#0be881"],
+                  stops: [0, 100],
+                },
+              },
+              colors: ["#0fbcf9"],
+              stroke: {
+                curve: "smooth",
+                width: 3,
+              },
+              tooltip: {
+                y: {
+                  formatter: (v) => `${v.toFixed(2)}`,
+                },
+              },
+            }}
+          />
+        )}
+      </div>
     </>
   );
 }
--- crypto-tracker/src/routes/Coin.tsx
+++ crypto-tracker/src/routes/Coin.tsx
@@ -12,7 +12,7 @@ import Chart from "./Chart";
 import Price from "./Price";
 import { useQuery } from "react-query";
 import { fetchCoinInfo, fetchCoinTickers } from "../api";
-
+import { Helmet } from "react-helmet";
 const Container = styled.div`
   padding: 0px 20px;
   max-width: 480px;
@@ -153,12 +153,20 @@ function Coin() {
   );
   const { isLoading: tickersLoading, data: tickersData } = useQuery<PriceData>(
     ["tickers", coinId],
-    () => fetchCoinTickers(coinId)
+    () => fetchCoinTickers(coinId),
+    {
+      refetchInterval: 5000,
+    }
   );
   const loading = infoLoading || tickersLoading;
   return (
     <>
       <Container>
+        <Helmet>
+          <title>
+            {state?.name ? state.name : loading ? "Loading..." : infoData?.name}
+          </title>
+        </Helmet>
         <Header>
           <Title>
             {state?.name ? state.name : loading ? "Loading..." : infoData?.name}
@@ -178,8 +186,8 @@ function Coin() {
                 <span>${infoData?.symbol}</span>
               </OverviewItem>
               <OverviewItem>
-                <span>Open Source:</span>
-                <span>{infoData?.open_source ? "Yes" : "No"}</span>
+                <span>Price:</span>
+                <span>{tickersData?.quotes.USD.price.toFixed(2)}</span>
               </OverviewItem>
             </Overview>
             <Description>{infoData?.description}</Description>
@@ -204,7 +212,7 @@ function Coin() {
 
             <Switch>
               <Route path={`/:coinId/chart`}>
-                <Chart />
+                <Chart coinId={coinId} />
               </Route>
               <Route path={`/:coinId/price`}>
                 <Price />
