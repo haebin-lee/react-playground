05582633922aaeab8aa94f65e550ab1bdd2a96ec
haebin.lee <luckylucy0717@gmail.com>
2024-08-20 13:00:35 -0700

    react-hook-form practice

--- todo/src/TodoList.tsx
+++ todo/src/TodoList.tsx
@@ -1,22 +1,75 @@
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+
+// function ToDoList() {
+//   const [todo, setTodo] = useState("");
+//   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+//     const {
+//       currentTarget: { value },
+//     } = event;
+//     setTodo(value);
+//   };
+//   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+//     event.preventDefault();
+//   };
+//   return (
+//     <div>
+//       <form onSubmit={onSubmit}>
+//         <input onChange={onChange} value={todo} placeholder="Write a to do " />
+//         <button>Add</button>
+//       </form>
+//     </div>
+//   );
+// }
+
+interface IForm {
+  email: string;
+  firstName: string;
+  extraError?: string;
+}
 
 function ToDoList() {
-  const [todo, setTodo] = useState("");
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setTodo(value);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError,
+  } = useForm<IForm>();
+  const onValid = (data: any) => {
+    console.log(data);
+    setError("extraError", { message: "Server is shut down." });
   };
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log(todo);
-  };
+  console.log(errors);
+
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input onChange={onChange} value={todo} placeholder="Write a to do " />
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={handleSubmit(onValid)}
+      >
+        <input
+          {...register("email", {
+            required: "Email is required",
+            pattern: {
+              value: /^[a-zA-Z0-9._%+-]+@gmail.com$/,
+              message: "Only gmail.com is allowed",
+            },
+          })}
+          placeholder="Email"
+        />
+        <span>{errors?.email?.message?.toString()}</span>
+        <input
+          {...register("firstName", {
+            validate: {
+              noLucy: (v) => (!v.includes("lucy") ? "No lucy includes" : true),
+            },
+          })}
+          placeholder="First Name"
+        />
+        <span>{errors?.firstName?.message?.toString()}</span>
+
         <button>Add</button>
+        <span>{errors?.extraError?.message?.toString()}</span>
       </form>
     </div>
   );
