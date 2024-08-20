59acf1dd1464e0eef09dcd81302e4b20d581b010
haebin.lee <luckylucy0717@gmail.com>
2024-08-20 00:00:40 -0700

    initialize todo project and add TodoList Component

---
+++ todo/src/theme.ts
@@ -0,0 +1,8 @@
+import { DefaultTheme } from "styled-components/dist/types";
+
+export const theme: DefaultTheme = {
+  bgColor: "#2d3436",
+  textColor: "#dfe6e9",
+  accentColor: "#fdcb6e",
+  cardBg: "transparent",
+};
